dac71a287df1ad6026e3beb0afe43ee66c56ddff
analys-mac <3064014960@qq.com>
2026-06-13 01:12:55 +0800

    5.2章节的开头部分

---
+++ 03-computer/编程语言的学习/python/test.py
@@ -0,0 +1,81 @@
+'''print(100 + 200)'''
+
+'''print('hello world')'''
+
+'''print(2 ** 10)'''
+
+'''print('the quick brown box', 'jumps over', 'the lazy dog')
+'''
+'''
+name = input()
+print(name)'''
+
+'''print('please enter your name:')
+print(1024 * 768)'''
+
+'''a = 100
+if a >= 0:
+    print(a)
+else :
+    print(a)'''
+
+'''print('i \'m "ok\"')'''
+
+'''print('i \'m learning\n python.')
+print('\\n\\')'''
+
+'''print(3 > 2)
+print(3 > 5)'''
+
+'''print(True and True)
+print(True and False)
+print(False and False)
+print(5 > 3 and 3 > 1)'''
+
+'''print(True or True)
+print(True or False)
+print(False or False)
+print(5 > 3 or 3 > 1)'''
+
+'''print(not True)
+print(not False)
+print(not 1 > 2)'''
+
+'''age = int(input())
+if age >= 18:
+    print('adult')
+else :
+    print('teenager')
+#注意数据类型的转换'''
+
+'''a = 1
+t_007 = 't007'
+answer = True
+print(a)
+a = 123
+print(a)
+a = 'abc'
+print(a)
+#变量本身类型不固定的语言成为动态语言，与之对应的是静态语言'''
+
+'''a = 'abc'
+1. 在内存中创建了一个字符串
+2. 在内存中创建了一个变量，并且指向了上面的字符串。
+'''
+
+'''a = 'abc'
+b = a
+a = 'xyz'
+print(b)'''
+
+'''n = 123
+f = 456.789
+s1 = 'hello, world'
+s2 = 'hello, \\\'adam\'\\'
+print(n)
+print(f)
+print(s1)
+print(s2)
+'''
+
+print(2 ** 16)
